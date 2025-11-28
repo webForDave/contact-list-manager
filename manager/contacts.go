a5cab1c2ef8611624fad0558ea35ffda1011f1c3
David Akinola <dakinola54@gmail.com>
2025-11-28 13:29:00 +0100

    Functionality for adding and viewing contacts

--- manager/contacts.go
+++ manager/contacts.go
@@ -1,3 +1,48 @@
 // Package manager models a simple contact list management system.
 // Enables CRUD operations on contacts.
 package manager
+
+import (
+	"errors"
+	"strings"
+)
+
+// Contact defines the blueprint for a contact.
+// A contact will have a name, an email address and a phone number.
+type Contact struct{
+	Name string
+	Email string
+	Phone string
+}
+
+// ContactList holds a list of contacts and the total number of contacts.
+type ContactList struct{
+	Contacts []Contact // slice of type "Contact".
+	Total int
+}
+
+// AddContact method creates a new contact in the ContactList struct.
+// It uses a pointer receiver because it modifies the Contacts field of the ContactList.
+// It returns an error if any validation breaks during the creation of a contact.
+func (cl *ContactList) AddContact(contact Contact) error{
+	// Checks for empty fields in the contact information.
+	if contact.Name == "" || contact.Email == "" || contact.Phone == ""{
+		return errors.New("invalid fields")
+	}
+	// Checks whether the email field has a valid email address.
+	if !strings.HasSuffix(contact.Email, "@gmail.com"){
+		return errors.New("invalid email address")
+	}
+	// Checks whether the phone number has a value less than 10 or greater than 11.
+	if len(contact.Phone) > 11 || len(contact.Phone) < 10{
+		return errors.New("invalid phone number length (>=10 n <=11)")
+	}
+
+	cl.Contacts = append(cl.Contacts, contact)
+
+	return nil
+}
+
+func (cl *ContactList) GetAllContacts() []Contact{
+	return cl.Contacts
+}
--- manager/contacts_test.go
+++ manager/contacts_test.go
@@ -1,3 +1,101 @@
 // Package manager models a simple contact list management system.
-// Enables CRUD operations on contacts. 
+// Enables CRUD operations on contacts.
 package manager
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestContact(t *testing.T){
+
+	t.Run("Test contact", func(t *testing.T){
+		contact := Contact{}
+
+		got := contact.Name
+		expect := ""
+
+		if got != expect{
+			t.Errorf("Got: %q, Expect: %q", got, expect)
+		}
+	})
+
+	t.Run("Validate empty fields", func(t *testing.T){
+		contact := Contact{Name: "", Email: "dakinola54@gmail.com", Phone: ""}
+		list := ContactList{}
+
+		err := list.AddContact(contact)
+
+		if err == nil  {
+			t.Errorf("expected an error but found none")
+		}
+
+	})
+
+	t.Run("Validate invalid email address", func(t *testing.T){
+		contact := Contact{Name: "David", Email: "dakinola54", Phone: "09077565788"}
+		list := ContactList{}
+
+		err := list.AddContact(contact)
+
+		if err == nil  {
+			t.Errorf("expected an error but found none")
+		}
+
+	})
+
+	t.Run("Validate crrect phone number length", func(t *testing.T){
+		contact := Contact{Name: "David", Email: "dakinola54@gmail.com", Phone: "090775657"}
+		list := ContactList{}
+
+		err := list.AddContact(contact)
+
+		if err == nil  {
+			t.Errorf("expected an error but found none")
+		}
+
+	})
+
+	t.Run("Add a new contact", func(t *testing.T){
+		contact := Contact{Name: "David", Email: "dakinola54@gmail.com", Phone: "09077565788"}
+		list := ContactList{}
+
+		err := list.AddContact(contact)
+
+		if err != nil{
+			t.Errorf("%q", err)
+		}
+		
+	})
+}
+
+func TestContactList(t *testing.T){
+	t.Run("Empty contact list", func(t *testing.T){
+		list := ContactList{}
+
+		got := list.Contacts
+		expect := []Contact{}
+
+		if !slices.Equal(got, expect){
+			t.Errorf("Got: %v, Expect: %v", got, expect)
+		}
+	})
+
+	t.Run("All contacts", func(t *testing.T){
+		contact1 := Contact{Name: "David", Email: "dakinola54@gmail.com", Phone: "09077565788"}
+		contact2 := Contact{Name: "Ken", Email: "talktoken@gmail.com", Phone: "08169195589"}
+
+		list := ContactList{}
+
+		list.AddContact(contact1)
+		list.AddContact(contact2)
+
+		allContacts := list.GetAllContacts()
+		expect := []Contact{contact1, contact2}
+
+		if !slices.Equal(allContacts, expect){
+			t.Errorf("Got: %v Expect: %v ", allContacts, expect)
+		}
+
+	})
+}
