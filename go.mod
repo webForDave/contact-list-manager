532f1946aa7baa164d472f9990fc716788162498
David Akinola <dakinola54@gmail.com>
2025-11-28 05:58:18 +0100

    Initial config

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/webfordave/contact-list-manager
+
+go 1.22.2
---
+++ manager/contacts.go
@@ -0,0 +1,3 @@
+// Package manager models a simple contact list management system.
+// Enables CRUD operations on contacts.
+package manager
